16358cdecfea32c839267274bfd441d4ce464879
SHAIK ISMAIL <97820658+shaikismail39@users.noreply.github.com>
2024-06-29 13:00:02 +0530

    Added backend api to SignUp.js

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -8,7 +8,7 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:4000/api/createuser`, {
+    const response = await fetch(`https://gofood-backend-2.onrender.com/api/createuser`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
